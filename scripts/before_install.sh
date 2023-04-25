1d6b2d598ebb1671ad26d7b8dccbc5907fc9b1ff
Herbert Ssevume <you@example.com>
2023-04-25 18:38:24 +0100

    3 commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,9 +7,9 @@ nvm install node
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
+# if [ -d "$DIR" ]; then
+#   echo "${DIR} exists"
+# else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
